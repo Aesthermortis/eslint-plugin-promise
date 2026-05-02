66057b5077d210f3efbcfaa68a51a64029676c24
Aesthermortis <17597833+Aesthermortis@users.noreply.github.com>
2026-05-02 08:11:36 +0200

    refactor(config): extract recommended rules config

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import packageJson from "../package.json" with { type: "json" };
+import recommendedRules from "./configs/recommended.js";
 import rules from "./rules/index.js";
 
 const { name: packageName, version: packageVersion } = packageJson;
 const namespace = "promise";
-
-/** @type {import("eslint").Linter.RulesRecord} */
-const recommendedRules = {
-  "promise/always-return": "error",
-  "promise/no-return-wrap": "error",
-  "promise/param-names": "error",
-  "promise/catch-or-return": "error",
-  "promise/no-native": "off",
-  "promise/no-nesting": "warn",
-  "promise/no-promise-in-callback": "warn",
-  "promise/no-callback-in-promise": "warn",
-  "promise/avoid-new": "off",
-  "promise/no-new-statics": "error",
-  "promise/no-return-in-finally": "warn",
-  "promise/valid-params": "warn",
-};
 
 /** @type {Record<string, import("eslint").Linter.Config>} */
 const configs = {};
