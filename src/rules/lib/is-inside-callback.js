672cab35735d06407d8bbac442c68265e91e433a
Aesthermortis <17597833+Aesthermortis@users.noreply.github.com>
2026-05-14 15:53:18 +0200

    fix(types): narrow callback function params

--- src/rules/lib/is-inside-callback.js
+++ src/rules/lib/is-inside-callback.js
@@ -8,18 +8,22 @@ import isInsidePromise from "./is-inside-promise.js";
  * @returns {boolean | undefined} Whether the node is inside a callback, or `undefined` when promises are allowed.
  */
 function isInsideCallback(node, exemptDeclarations) {
-  const isFunction =
-    node.type === "FunctionExpression" ||
-    node.type === "ArrowFunctionExpression" ||
-    (!exemptDeclarations && node.type === "FunctionDeclaration"); // this may be controversial
-
   // it's totally fine to use promises inside promises
-  if (isInsidePromise(node)) return;
+  if (isInsidePromise(node)) {
+    return;
+  }
 
-  const name = node.params && node.params[0] && node.params[0].name;
+  if (
+    node.type !== "FunctionExpression" &&
+    node.type !== "ArrowFunctionExpression" &&
+    (exemptDeclarations || node.type !== "FunctionDeclaration")
+  ) {
+    return false;
+  }
+
+  const name = node.params[0]?.type === "Identifier" ? node.params[0].name : undefined;
   const firstArgIsError = name === "err" || name === "error";
-  const isInACallback = isFunction && firstArgIsError;
-  return isInACallback;
+  return firstArgIsError;
 }
 
 export default isInsideCallback;
