036a4bec64eb5a5e8a465739fca15717a979d7c3
Aesthermortis <17597833+Aesthermortis@users.noreply.github.com>
2026-05-14 15:58:45 +0200

    fix(types): guard promise member detection

--- src/rules/lib/is-promise.js
+++ src/rules/lib/is-promise.js
@@ -1,36 +1,43 @@
 import PROMISE_STATICS from "./promise-statics.js";
 
 /**
+ * Get the identifier name from a member expression property.
+ *
+ * @param {import("estree").MemberExpression} expression - Member expression to inspect.
+ * @returns {string | undefined} Property name when the member uses an identifier.
+ */
+function getMemberPropertyName(expression) {
+  return expression.property.type === "Identifier" ? expression.property.name : undefined;
+}
+
+/**
  * Check whether an expression belongs to a Promise chain or Promise static call.
  *
- * @param {import("eslint").Rule.Node} expression - The AST expression node to evaluate.
+ * @param {import("estree").Node} expression - The AST expression node to evaluate.
  * @returns {boolean} Whether the expression is recognized as promise-related.
  */
 function isPromise(expression) {
+  if (expression.type !== "CallExpression" || expression.callee.type !== "MemberExpression") {
+    return false;
+  }
+
+  const propertyName = getMemberPropertyName(expression.callee);
+
   return (
     // hello.then()
-    (expression.type === "CallExpression" &&
-      expression.callee.type === "MemberExpression" &&
-      expression.callee.property.name === "then") ||
+    propertyName === "then" ||
     // hello.catch()
-    (expression.type === "CallExpression" &&
-      expression.callee.type === "MemberExpression" &&
-      expression.callee.property.name === "catch") ||
+    propertyName === "catch" ||
     // hello.finally()
-    (expression.type === "CallExpression" &&
-      expression.callee.type === "MemberExpression" &&
-      expression.callee.property.name === "finally") ||
+    propertyName === "finally" ||
     // somePromise.ANYTHING()
-    (expression.type === "CallExpression" &&
-      expression.callee.type === "MemberExpression" &&
-      isPromise(expression.callee.object)) ||
+    (expression.callee.object.type !== "Super" && isPromise(expression.callee.object)) ||
     // Promise.STATIC_METHOD()
-    (expression.type === "CallExpression" &&
-      expression.callee.type === "MemberExpression" &&
+    (propertyName !== undefined &&
       expression.callee.object.type === "Identifier" &&
       expression.callee.object.name === "Promise" &&
-      PROMISE_STATICS.has(expression.callee.property.name) &&
-      expression.callee.property.name !== "withResolvers")
+      PROMISE_STATICS.has(propertyName) &&
+      propertyName !== "withResolvers")
   );
 }
 
