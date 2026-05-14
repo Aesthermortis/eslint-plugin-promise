6c2a0600aeab8fcd60687718574a2c4f58d22d1d
Aesthermortis <17597833+Aesthermortis@users.noreply.github.com>
2026-05-14 15:51:21 +0200

    fix(types): type callback call helpers

--- src/rules/lib/is-callback.js
+++ src/rules/lib/is-callback.js
@@ -3,17 +3,16 @@ import isNamedCallback from "./is-named-callback.js";
 /**
  * Check if an AST node represents a callback function call.
  *
- * @param {import("eslint").Rule.Node} node - The AST node to check.
+ * @param {import("estree").CallExpression} node - The call expression to check.
  * @param {string[]} exceptions - Callback names to exclude from the check.
  * @returns {boolean} Whether the node is a callback call expression.
  */
 function isCallback(node, exceptions) {
-  const isCallExpression = node.type === "CallExpression";
-  // istanbul ignore next -- always invoked on `CallExpression`
-  const callee = node.callee || {};
-  const nameIsCallback = isNamedCallback(callee.name, exceptions);
-  const isCB = isCallExpression && nameIsCallback;
-  return isCB;
+  if (node.callee.type !== "Identifier") {
+    return false;
+  }
+
+  return isNamedCallback(node.callee.name, exceptions);
 }
 
 export default isCallback;
